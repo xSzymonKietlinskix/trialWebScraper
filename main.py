3bde1f6e58fb42f7563550c592d029197f668911
xSzymonKietlinskix <szymon.kietlinski@gmail.com>
2023-11-26 21:40:42 +0100

    Exporting brands and models to .csv

--- main.py
+++ main.py
@@ -7,26 +7,31 @@ from selenium.webdriver.common.by import By
 import chromedriver_binary
 import time
 import csv
-def getWebsite():
-    url = "https://www.otomoto.pl/"
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
+def save_to_csv(fileName, list):
+    header = ['brand']
+    with open(fileName, 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
+        for l in list:
+            writer.writerow([l])
 
-        #print("Zawartość strony: \n")
-        #print(response.text)
-        return response
-    except requests.exceptions.RequestException as e:
-        print(f"Błąd podczas wykonywania żądania: {e}")
-        return None
-    except requests.exceptions.HTTPError as e:
-        print(f"Błąd http: {e}")
-        return None
+def save_models_to_csv(fileName, list, marka):
+    with open(fileName, 'a', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
+        for l in list:
+            if l == "Wszytskie modele" :
+                continue
+            row = marka + ';' + l
+            writer.writerow([row])
 
-#czytanie stony
-def readWebsite(page, min_amount):
-
-
+def read_from_csv(fileName):
+    list = []
+    with open(fileName, 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            list.append(row)
+    return list
+def read_marki(min_amount):
     driver = webdriver.Chrome()
     driver.maximize_window()
     # Otwarcie strony internetowej
@@ -47,87 +52,69 @@ def readWebsite(page, min_amount):
     list_marki = driver.find_element(By.XPATH,
                                 '//*[@id="__next"]/div/div/div/div[2]/div[1]/div/form/section/div/div[1]/div/ul')
     marki = list_marki.find_elements(By.TAG_NAME, 'li')
+    istotne_marki = []
+
     for marka in marki:
-        print("TEST")
-        print(marka.text)
-        print(len(marki))
         l_egz = r'\((.*?)\)'
         if not marka.text == "Wszystkie marki":
             dopasowanie = re.search(l_egz, marka.text)
             if int(dopasowanie.group(1)) > min_amount:
-                print(dopasowanie.group(1))
-                print(marka.text)
-                test = marka.find_element(By.TAG_NAME, 'input')
-                if not test.is_selected():
-                    test.click()
-                    brands_close = driver.find_element(By.XPATH, '//*[@id="__next"]/div/div/div/div[2]/div[1]/div/form/section/div/div[1]/div/div/span/button')
-                    brands_close.click()
-                    models_open = driver.find_element(By.XPATH,'//*[@id="__next"]/div/div/div/div[2]/div[1]/div/form/section/div/div[2]')
-                    models_open.click()
-                    lista_modeli = models_open.find_elements(By.XPATH,'//*[@id="__next"]/div/div/div/div[2]/div[1]/div/form/section/div/div[2]/div/ul')
-                    for model in lista_modeli:
-                         print(model.text)
-                    models_close = driver.find_element(By.XPATH,
-                                                       '/html/body/div[1]/div/div/div/div[2]/div[1]/div/form/section/div/div[2]/div/div/span/button')
-                    # time.sleep(2)
-                    # models_close.click()
+                istotne_marki.append(marka.text)
 
-                    # time.sleep(2)
-
-                    brand_cancel = driver.find_element(By.XPATH,
-                                                       '//*[@id="__next"]/div/div/div/div[2]/div[1]/div/form/section/div/div[1]/div/div/span/button')
-                    brand_cancel.click()
-                    time.sleep(2)
-                    brands_open2 = driver.find_element(By.XPATH,
-                                                       '//*[@id="__next"]/div/div/div/div[2]/div[1]/div/form/section/div/div[1]/div/div/span/button')
-                    brands_open2.click()
-                    list_marki = driver.find_element(By.XPATH,
-                                                     '//*[@id="__next"]/div/div/div/div[2]/div[1]/div/form/section/div/div[1]/div/ul')
-                    marki = list_marki.find_elements(By.TAG_NAME, 'li')
-
-
-
-    # for marka in marki:
-    #
-    #     print(marka.text)
-    #     select_marka = marka.find_element(By.XPATH, '//*[@id="__next"]/div/div/div/div[2]/div[1]/div/form/section/div/div[1]/div/ul/li[2]/div/label/input')
-    #     select_marka.click()
-    #     brands_close = driver.find_element(By.XPATH, '//*[@id="__next"]/div/div/div/div[2]/div[1]/div/form/section/div/div[1]/div/div/span/button')
-    #     brands_close.click()
-    #     models_open = driver.find_element(By.XPATH,
-    #                                    '//*[@id="__next"]/div/div/div/div[2]/div[1]/div/form/section/div/div[2]')
-    #     models_open.click()
-    #
-    #     lista_modeli = models_open.find_elements(By.XPATH,
-    #                                  '//*[@id="__next"]/div/div/div/div[2]/div[1]/div/form/section/div/div[2]/div/ul')
-    #     for model in lista_modeli:
-    #         print(model.text)
-    #
-        # models_close = driver.find_element(By.XPATH,
-        #                                    '//*[@id="__next"]/div/div/div/div[2]/div[1]/div/form/section/div/div[2]/div/fieldset/span/button')
-        # models_close.click()
-        #
-        # time.sleep(2)
-        #
-        # brand_cancel = driver.find_element(By.XPATH, '//*[@id="__next"]/div/div/div/div[2]/div[1]/div/form/section/div/div[1]/div/div/span/button')
-        # brand_cancel.click()
-        # time.sleep(2)
-        # brands_open2 = driver.find_element(By.XPATH, '//*[@id="__next"]/div/div/div/div[2]/div[1]/div/form/section/div/div[1]/div/div/span/button')
-        # brands_open2.click()
-
-
-
-
+    print(istotne_marki)
+    return istotne_marki
 
 
 
     driver.quit()
+
+def read_models(marki, min_amount):
+    istotne_modele = []
+    for marka_ in marki:
+        if marka_[0] == "brand" or marka_[0] == "Alfa Romeo (1456)":
+            continue
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        # Otwarcie strony internetowej
+        link = 'https://www.otomoto.pl/osobowe/'
+        marka = marka_[0]
+        marka = marka.lower()
+
+        marka = re.sub(r'\([^)]*\)', '', marka)
+        marka = marka.rstrip()
+        marka = marka.replace(" ", "_")
+        link += marka
+        link += '?search%5Badvanced_search_expanded%5D=true'
+        driver.get(link)
+        # Kliknięcie ciasteczek
+        cookies = driver.find_element(By.XPATH,
+                                      '//*[@id="onetrust-accept-btn-handler"]')
+
+        cookies.click()
+        models_open = driver.find_element(By.XPATH,
+                                         '//*[@id="__next"]/div/div/div/div[2]/div[1]/div/form/section/div/div[2]')
+        models_open.click()
+        lista_modeli = models_open.find_element(By.XPATH,
+                                        '//*[@id="__next"]/div/div/div/div[2]/div[1]/div/form/section/div/div[2]/div/ul')
+        modele = lista_modeli.find_elements(By.TAG_NAME, 'li')
+        print(modele)
+        for l in modele:
+            istotne_modele.append(l.text)
+        save_models_to_csv('modele.csv', istotne_modele, marka)
+
+
+
+    return istotne_modele
 
 
 
 
 
 if __name__ == '__main__':
-    page = getWebsite()
-    readWebsite(page,100)
+    #marki = read_marki(100)
+    #save_to_csv('marki.csv', marki)
+    marki_lista = read_from_csv('marki.csv')
+    modele = read_models(marki_lista,100)
+
+
 
